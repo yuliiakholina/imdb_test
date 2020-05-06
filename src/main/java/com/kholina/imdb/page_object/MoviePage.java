238a055322c065fe3640935e244d5ce23a19249f
Yuliia Kholina <ykhol@softserveinc.com>
2020-05-06 14:14:57 +0300

    Stabilize tests with waiters

--- src/main/java/com/kholina/imdb/page_object/MoviePage.java
+++ src/main/java/com/kholina/imdb/page_object/MoviePage.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.kholina.imdb.constant.Constant.DEFAULT_WAIT_TIME;
 
 public class MoviePage {
     private SelenideElement movieTitle = $(".title_wrapper > h1");
@@ -13,6 +14,6 @@ public class MoviePage {
 
     public boolean verifyUserIsOnMoviePage(String movieName) {
         log.info("Verify user is on Sign In page");
-        return movieTitle.waitUntil(Condition.visible,7000).getText().contains(movieName);
+        return movieTitle.waitUntil(Condition.visible,DEFAULT_WAIT_TIME).getText().contains(movieName);
     }
 }
